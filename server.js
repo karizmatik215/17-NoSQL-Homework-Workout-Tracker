9d96bf1cae6831031083d9400e81f288b44b2314
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-21 03:38:24 -0400

    added express, mongoose, morgan, mongodb connection and app routes

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/upenn', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(require('./routes/apiRoutes'));
+app.use(require('./routes/htmlRoutes'));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
