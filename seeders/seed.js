276f17a7abdd99e7ff1142227b5136184d18c002
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-22 19:33:28 -0400

    added unified topology

--- seeders/seed.js
+++ seeders/seed.js
@@ -4,6 +4,7 @@ let db = require('../models');
 mongoose.connect('mongodb://localhost/fitnesstracker', {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 let workoutSeed = [
